3e4ea73d6ea2878718a6e6a68e235500eec2bd7d
Hexa Quebec <hexaquebec80@gmail.com>
2026-06-12 14:29:56 -0400

    Ajout PostgreSQL

--- GafouShop/settings.py
+++ GafouShop/settings.py
@@ -111,27 +111,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'GafouShop.wsgi.application'
 
+import os
+import dj_database_url
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
-}
-
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
-
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
